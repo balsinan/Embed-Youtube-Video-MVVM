9adba3b3b326b9db641055182e06b2200a30603d
Sinan <sinan@sinan-mbp.local>
2021-05-22 21:32:57 +0300

    Add error handling

--- Embed Youtube Video/Networking/YoutubeApi.swift
+++ Embed Youtube Video/Networking/YoutubeApi.swift
@@ -13,7 +13,7 @@ let YoutubeVideoDetailApi = "https://www.googleapis.com/youtube/v3/videos"
 
 class YoutubeApi{
     static let sharedInstance = YoutubeApi()
-    public typealias onCompleteVideoHandler = (([YoutubeVideoObject]?,AFError?)->())
+    public typealias onCompleteVideoHandler = (([YoutubeVideoObject]?,NetworkingErrors?)->())
     
     func fetchChannelVideoList(channelId : String, apiKey : String, onComplete:@escaping onCompleteVideoHandler){
         
@@ -23,26 +23,38 @@ class YoutubeApi{
                                      "key" : apiKey]
         
         AF.request(YoutubePlaylistApi, method:.get, parameters: parameter).responseJSON { (response) in
-            
             switch response.result {
-            
             case .success:
-                let data = response.data
-                let youtubeVideo = try? JSONDecoder().decode(YoutubeVideoModel.self, from: data!)
-                var VideoArray : [YoutubeVideoObject] = []
-                if let videoModel = youtubeVideo{
-                    for item in videoModel.items{
-                        let videoObject = YoutubeVideoObject(title: item.snippet.title, imageUrl: item.snippet.thumbnails.high.url, duration: "", videoId: item.snippet.resourceID.videoID)
-                        VideoArray.append(videoObject)
+                let statusCode = response.response?.statusCode
+                switch statusCode {
+                case 200:
+                    let data = response.data
+                    let youtubeVideo = try? JSONDecoder().decode(YoutubeVideoModel.self, from: data!)
+                    var VideoArray : [YoutubeVideoObject] = []
+                    if let videoModel = youtubeVideo{
+                        for item in videoModel.items{
+                            let videoObject = YoutubeVideoObject(title: item.snippet.title, imageUrl: item.snippet.thumbnails.high.url, duration: "", videoId: item.snippet.resourceID.videoID)
+                            VideoArray.append(videoObject)
+                        }
                     }
+                    onComplete(VideoArray, nil)
+                    break
+                case 400:
+                    onComplete(nil, .playlistOperationUnsupported)
+                    break
+                case 403:
+                    onComplete(nil, .playlistItemsNotAccessible)
+                    break
+                case 404:
+                    onComplete(nil, .playlistNotFound)
+                    break
+                default:
+                    break
                 }
-                onComplete(VideoArray, nil)
-                break
-                
+        
             case .failure:
-                onComplete(nil, response.error)
+                onComplete(nil, .failure)
                 break
-                
             }
         }.resume()
     }
@@ -95,9 +107,9 @@ class YoutubeApi{
             let timeUnit: String = videoDurationString.substring(to: 1)
 
             if (timeUnit == "H") {
-                hours = Int(part as! String)!
+                hours = Int(part! as String)!
             }else if (timeUnit == "M") {
-                minutes = Int(part as! String)!
+                minutes = Int(part! as String)!
             }else if (timeUnit == "S") {
                 seconds   = Int(part! as String)!
             }
@@ -108,4 +120,37 @@ class YoutubeApi{
         }
         return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
     }
+    
+    enum NetworkingErrors : Error{
+        case playlistItemsNotAccessible
+        case watchHistoryNotAccessible
+        case watchLaterNotAccessible
+        case playlistNotFound
+        case videoNotFound
+        case playlistIdRequired
+        case playlistOperationUnsupported
+        case failure
+        
+        var localizedDescription : String{
+            switch self{
+            case .playlistItemsNotAccessible:
+                return "The request is not properly authorized to retrieve the specified playlist."
+            case .watchHistoryNotAccessible:
+                return "Watch history data cannot be retrieved through the API."
+            case .watchLaterNotAccessible:
+                return "Items in watch later playlists cannot be retrieved through the API."
+            case .playlistNotFound:
+                return "The playlist identified with the request's playlistId parameter cannot be found."
+            case .videoNotFound:
+                return "The video identified with the request's videoId parameter cannot be found."
+            case .playlistIdRequired:
+                return "The subscribe request does not specify a value for the required playlistId property."
+            case .playlistOperationUnsupported:
+                return "The API does not support the ability to list videos in the specified playlist. For example, you can't list a video in your watch later playlist."
+            case .failure:
+                return "Failure."
+            }
+        }
+    }
 }
+
